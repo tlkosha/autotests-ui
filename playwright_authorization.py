e90bfdc9e785635e37a45c027bb4fbdf9a3c2237
tlkosha <w21151101@gmail.com>
2026-04-13 22:17:25 +0700

    playwright

--- playwright_authorization.py
+++ playwright_authorization.py
@@ -6,16 +6,16 @@ with sync_playwright() as playwright:
 
     page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
 
-    email_input = page.locator('//div[@data-testid="login-form-email-input"]//div//input')
+    email_input = page.get_by_test_id('login-form-email-input').locator('input')
     email_input.fill('user.name@gmail.com')
 
-    password_input = page.locator('//div[@data-testid="login-form-password-input"]//div//input')
+    password_input = page.get_by_test_id('login-form-password-input').locator('input')
     password_input.fill('password')
 
-    login_button = page.locator('//button[@data-testid="login-page-login-button"]')
+    login_button = page.get_by_test_id('login-page-login-button')
     login_button.click()
 
-    wrong_email_or_password_alert = page.locator('//div[@data-testid = "login-page-wrong-email-or-password-alert"]')
+    wrong_email_or_password_alert = page.get_by_test_id('login-page-wrong-email-or-password-alert')
     expect(wrong_email_or_password_alert).to_be_visible() #проверка что элемент видимый
     expect(wrong_email_or_password_alert).to_have_text("Wrong email or password") #проверка что элемент содержит текст
 
