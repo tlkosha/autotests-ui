8cc170f3cced6ed68b5663413261e1148bea11b2
tlkosha <w21151101@gmail.com>
2026-04-21 17:31:28 +0700

    плагины pytest

---
+++ conftest.py
@@ -0,0 +1,3 @@
+pytest_plagins = (
+    "fixtures.browsers"
+)
--- tests/conftest.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from playwright.sync_api import sync_playwright, Page
-
-
-@pytest.fixture
-def chromium_page(playwright) -> Page:
-        browser = playwright.chromium.launch(headless=False)  # headless=False - значит запуск браузера с интерфейсом
-        yield browser.new_page()
